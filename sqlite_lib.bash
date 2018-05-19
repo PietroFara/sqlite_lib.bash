b39bba3ac42986c0ebc29756d3eafc67e422aba8
curaro <curaro.arsenico@gmail.com>
2018-05-19 02:19:28 +0200

    Fixing the mutliple line column bug
    Thanks to Fmstrat for the segnalation
    TODO: Fixing column with blank new lines

--- sqlite_lib.bash
+++ sqlite_lib.bash
@@ -27,8 +27,12 @@ function sqlite_create_fetch_array() {
 		while sqlite_read_line; do
 			if [[ ! $COL_NAME ]];then
 				((num_rows=num_rows+1))
-			fi
-	       	rows[$num_rows,"$COL_NAME"]="$DATA"
+			elif [[ ! $DATA ]];then #if we cannot find DATA, we can assume that the previous column value has a newline character
+				rows[$num_rows,"$PREV_COL_NAME"]=${rows[$num_rows,"$PREV_COL_NAME"]}$'\n'$COL_NAME #COL_NAME contains DATA in this case
+			else
+	       		rows[$num_rows,"$COL_NAME"]="$DATA"
+	       		PREV_COL_NAME=$COL_NAME #saving the previous column name in case we have a multiple line column
+	       	fi
 		done <<< "$result"
 		((num_rows=num_rows+1))
 	fi
